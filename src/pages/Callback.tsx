8f199653adb0ba3e33ba67accdee42b005e6c000
Jacob <sevenskillz@gmail.com>
2025-09-14 13:40:42 -0400

    small rename to folder

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -1,4 +1,3 @@
-// src/pages/Callback.tsx
 import { useEffect } from "react";
 import {UserManager, WebStorageStateStore} from "oidc-client-ts";
 
@@ -6,8 +5,8 @@ const oidcConfig =
 {
   authority: "https://dev-ttserecwbaauimqy.us.auth0.com",
   client_id: "tJVeFR6i8EIF1mID7tLET7fL61QifGkb",
-  redirect_uri: "http://localhost:5173/callback",
-  post_logout_redirect_uri: "http://192.168.1.1:5173",
+  redirect_uri: "http://192.168.1.143:5173/callback",
+  post_logout_redirect_uri: "http://192.168.1.143:5173",
   response_type: "code",
   scope:"openid profile email",
   userStore: new WebStorageStateStore({ store: window.localStorage })
