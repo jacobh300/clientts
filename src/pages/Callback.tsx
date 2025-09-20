652c0fe9e496f75399a791ec4bd1914f64bb4fd8
Jacob <sevenskillz@gmail.com>
2025-09-20 12:56:29 -0400

    fix

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -5,8 +5,8 @@ const oidcConfig =
 {
   authority: "https://dev-ttserecwbaauimqy.us.auth0.com",
   client_id: "tJVeFR6i8EIF1mID7tLET7fL61QifGkb",
-  redirect_uri: "http://192.168.1.143:5173/callback",
-  post_logout_redirect_uri: "http://192.168.1.143:5173",
+  redirect_uri: "https://clientts-git-master-jacobh300s-projects.vercel.app/callback",
+  post_logout_redirect_uri: "https://clientts-git-master-jacobh300s-projects.vercel.app",
   response_type: "code",
   scope:"openid profile email",
   userStore: new WebStorageStateStore({ store: window.localStorage })
